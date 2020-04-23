a3d27d7fcf5442dab621d3b3be9a99779f00db03
upieez <samuelhww@gmail.com>
2020-04-23 21:19:28 +0800

    add age

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 window.onload = (event) => {
     document.querySelector('body').classList.add('ready');
 };
+
+const today = new Date
+const birthday = new Date('November 23, 1994 07:42:32')
+const age = today.getFullYear() - birthday.getFullYear();
+
+document.body.innerHTML = document.body.innerHTML.replace('AGE', age);
