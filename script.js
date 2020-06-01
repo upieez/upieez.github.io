266ef2219e88fe638dda9de687832048e783d9c2
upieez <samuelhww@gmail.com>
2020-06-01 15:33:01 +0800

    destructure function from library

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.onload = (event) => {
 	document.querySelector("body").classList.add("ready");
 };
 
+const { annotate, annotationGroup } = RoughNotation;
+
 const today = new Date();
 const birthday = new Date("November 23, 1994 07:42:32");
 const age = today.getFullYear() - birthday.getFullYear();
