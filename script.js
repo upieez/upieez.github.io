6997ccf5ae6be81ca6e0261820038cd7d3bac18a
upieez <samuelhww@gmail.com>
2020-06-06 22:39:03 +0800

    fix software engineer overflower

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const age = today.getFullYear() - birthday.getFullYear();
 const greetings = [
 	`<p>I hope you are well and good fellow reader! I'm <span class="name" id="my-name">Samuel</span>, ${age} years old this year and I'm a tech enthusiast who happens to also be a <span class="alt-highlight" id="my-title">developer</span> with front & back end capability. I code, create and love to learn new things! Always excited for challenges.</p>`,
 	`<p>It's nice to meet you 🙂 My name is <span class="name" id="my-name">Sam</span>, ${age} this year and I'm a <span class="alt-highlight" id="my-title">software developer</span> with a huge passion for learning new and wonderful things. I code, create and have a big fluffy cat at home! Looking to better myself everyday.</p>`,
-	`<p>Hi there! Thanks for stopping by and checking me out. I'm <span class="name" id="my-name">Sammy</span>, ${age}, and I'm an animal loving <span class="alt-highlight" id="my-title">software engineer</span> who wants to get involved in all sorts of new technology. <br /> I code, create and love to learn new things! Trying my hands on leetcode at the moment.</p>`,
+	`<p>Hi there! Thanks for stopping by and checking me out. I'm <span class="name" id="my-name">Sammy</span>, ${age}, and I'm an animal loving <span class="alt-highlight" id="my-title">SWE</span> who wants to get involved in all sorts of new technology. <br /> I code, create and love to learn new things! Trying my hands on leetcode at the moment.</p>`,
 ];
 
 document.querySelector(".main-message").innerHTML =
